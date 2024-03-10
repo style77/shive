8ea262e729fe2608591e24050c4523ac93276ebf
style77 <stylek777@gmail.com>
2024-03-10 17:30:05 +0100

    chore(api): add allowed hosts to settings

--- api/settings.py
+++ api/settings.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic_settings import BaseSettings
 
 
@@ -5,6 +6,8 @@ class Settings(BaseSettings):
     APP_NAME: str = "Shive"
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+
+    ALLOWED_HOSTS: List[str] = ["*"]
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 5  # 5 minutes
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7  # 7 days
