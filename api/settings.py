80b2fb3b065a10ef01d252b5c290d82a34e0f292
style77 <stylek777@gmail.com>
2024-03-10 17:13:08 +0100

    chore(api): add access tokens expiration time

--- api/settings.py
+++ api/settings.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 5  # 5 minutes
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7  # 7 days
+
     DATABASE_URL: str = "postgresql://shive:shive@localhost:5432/shive"
 
     class Config:
