2c3898f75092da93fda950190149ce7490358f2d
style77 <stylek777@gmail.com>
2024-03-10 02:11:58 +0100

    refactor(api): uppercase fields in settings

--- api/settings.py
+++ api/settings.py
@@ -2,9 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = "Shive"
-    secret_key: str
-    algorithm: str = "HS256"
+    APP_NAME: str = "Shive"
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+
+    DATABASE_URL: str = "postgresql://shive:shive@localhost:5432/shive"
 
     class Config:
         env_file = ".env"
