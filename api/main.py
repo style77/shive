5c639a974b955df51b1d2f854499c930e73f0817
style77 <stylek777@gmail.com>
2024-03-10 17:30:20 +0100

    chore(api): setup app middlewares

--- api/main.py
+++ api/main.py
@@ -3,13 +3,17 @@ from contextlib import asynccontextmanager
 import graphene
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI, APIRouter
 from api.settings import settings
 from api.core.logs import logger
 
 from api.users.router import router as users_router
 from api.health.router import router as health_router
+from api.auth.router import router as auth_router
 from api.schema import Query, Mutation
+from api.auth.middleware import RefreshTokenMiddleware
 
 
 @asynccontextmanager
@@ -27,10 +31,21 @@ app = FastAPI(
     description=f"{settings.APP_NAME} API",
     lifespan=lifespan,
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_HOSTS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(RefreshTokenMiddleware)
+
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(users_router)
 router.include_router(health_router)
+router.include_router(auth_router)
 
 app.include_router(router)
 
