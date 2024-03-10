1b34224df60c73f4f7d1bfd85c4616cf99c19340
style77 <stylek777@gmail.com>
2024-03-10 03:26:58 +0100

    refactor(api): generate id function

--- api/common/models.py
+++ api/common/models.py
@@ -4,7 +4,11 @@ from datetime import datetime
 import uuid
 
 
+def generate_id() -> str:
+    return str(uuid.uuid4())
+
+
 class GeneralModel(SQLModel):
-    id: str = Field(default_factory=uuid.uuid4, primary_key=True, index=True, nullable=False)
+    id: str = Field(default_factory=generate_id, primary_key=True, index=True, nullable=False)
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: Optional[datetime] = Field(default=None, nullable=True)
