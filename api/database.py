d74ce64c397b20475c61acc798c90cbc3747ebd7
style77 <stylek777@gmail.com>
2024-03-10 02:10:54 +0100

    chore(api): database initialization

---
+++ api/database.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
+
+from api.settings import settings
+
+engine = AsyncEngine(create_engine(settings.DATABASE_URL, echo=True, future=True))
+
+
+async def init_table() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
