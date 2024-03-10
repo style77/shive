039670b754c8bb6bd2a8c206f078d5e2e53129e9
style77 <stylek777@gmail.com>
2024-03-10 02:10:24 +0100

    chore(api): users router

--- api/users/router.py
+++ api/users/router.py
@@ -1,9 +1,3 @@
 from fastapi import APIRouter
-from api.users.models import UserCreate, User
 
 router = APIRouter()
-
-
-@router.post("/", response_model=User)
-async def users(data: UserCreate):
-    return data
