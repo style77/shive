e76f6a3375ab10fe84f5fd17a48abfb2c9eb35fc
style77 <stylek777@gmail.com>
2024-03-10 03:25:55 +0100

    refactor(api): update database.py to use async_session as a global variable

--- api/database.py
+++ api/database.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from api.settings import settings
 
 engine = AsyncEngine(create_engine(settings.DATABASE_URL, echo=True, future=True))
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def init_table() -> None:
@@ -14,6 +15,5 @@ async def init_table() -> None:
 
 
 async def get_session() -> AsyncSession:
-    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session
