d7b1923188dffa52bbf80f4a9916c8bd3c0c021c
th11n <thinex@wp.pl>
2024-03-11 23:31:20 +0100

    chore(client): routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+import Login from './pages/Login.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <App />,
+  },
+  {
+      path: "/login",
+      element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
