75c87894e232c3431a0e035b21f809462763f997
style77 <stylek777@gmail.com>
2024-03-12 21:45:38 +0100

    fix(api): async pytest with injected test database session

--- api/conftest.py
+++ api/conftest.py
@@ -1,28 +1,41 @@
-from fastapi.testclient import TestClient
+import asyncio
 import pytest
 from sqlmodel import SQLModel, create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
+from httpx import AsyncClient
 from api.database import async_session
 from api.main import app
 from api.settings import settings
 
-engine = AsyncEngine(
-    create_engine(
-        settings.TEST_DATABASE_URL,
-        echo=False,
-        future=True,
-    )
-)
+
+@pytest.fixture(scope="module")
+def event_loop():
+
+    loop = asyncio.get_event_loop()
+
+    yield loop
 
 
 @pytest.fixture(scope="module")
-def client() -> TestClient:
-    with TestClient(app) as client:
+async def client() -> AsyncClient:
+    async with AsyncClient(app=app, base_url="http://shive_test") as client:
         yield client
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="module")
+def anyio_backend():
+    return 'asyncio'
+
+@pytest.fixture(scope="module", autouse=True)
 async def setup_session():
+    engine = AsyncEngine(
+        create_engine(
+            settings.TEST_DATABASE_URL,
+            echo=False,
+            future=True,
+        )
+    )
+
     async_session.configure(bind=engine)
 
     async with engine.begin() as conn:
