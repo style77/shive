db8db7d7a6b05bbb2d601f597f71f66be4d0f729
style77 <stylek777@gmail.com>
2024-03-13 00:12:50 +0100

    test(api): create user more test cases

--- api/users/tests/test_create_user.py
+++ api/users/tests/test_create_user.py
@@ -35,3 +35,77 @@ async def test_create_user(client: AsyncClient):
     assert "errors" not in data
     assert "data" in data
     assert "createUser" in data["data"]
+    assert data["data"]["createUser"]["__typename"] == "CreateUserSuccess"
+    assert data["data"]["createUser"]["user"]["username"] == "test"
+
+@pytest.mark.anyio
+async def test_create_user_fail_username_exists(client: AsyncClient):
+    mutation = """
+        mutation createUser {
+            createUser(email: "test@test.com", username: "test", fullName: "Test Test", password: "test") {
+                __typename
+                ... on CreateUserFailUsernameExists {
+                    suggestedAlternatives
+                    errorMessage
+                }
+                ... on CreateUserFailOther {
+                    errorMessage
+                }
+                ... on CreateUserSuccess {
+                    user {
+                        id
+                        username
+                        email
+                        fullName
+                    }
+                }
+            }
+        }
+        """
+    
+    response = await client.post("/graphql/", json={"query": mutation})
+    
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "errors" not in data
+    assert "data" in data
+    assert "createUser" in data["data"]
+    assert data["data"]["createUser"]["__typename"] == "CreateUserFailUsernameExists"
+    assert data["data"]["createUser"]["errorMessage"] == "Username already exists."
+
+@pytest.mark.anyio
+async def test_create_user_fail_email_exists(client: AsyncClient):
+    mutation = """
+        mutation createUser {
+            createUser(email: "test@test.com", username: "test1", fullName: "Test Test", password: "test") {
+                __typename
+                ... on CreateUserFailUsernameExists {
+                    suggestedAlternatives
+                    errorMessage
+                }
+                ... on CreateUserFailOther {
+                    errorMessage
+                }
+                ... on CreateUserSuccess {
+                    user {
+                        id
+                        username
+                        email
+                        fullName
+                    }
+                }
+            }
+        }
+        """
+    
+    response = await client.post("/graphql/", json={"query": mutation})
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "errors" not in data
+    assert "data" in data
+    assert "createUser" in data["data"]
+    assert data["data"]["createUser"]["__typename"] == "CreateUserFailOther"
+    assert data["data"]["createUser"]["errorMessage"] == "Email already exists."
