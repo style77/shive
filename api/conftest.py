5c92904f25b49ed188a263848f145499b74c7d9e
style77 <stylek777@gmail.com>
2024-03-12 15:30:33 +0100

    refactor(api): use test database url in conftest

--- api/conftest.py
+++ api/conftest.py
@@ -1,24 +1,14 @@
 from fastapi.testclient import TestClient
 import pytest
 from sqlmodel import SQLModel, create_engine
-from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine
 from api.database import async_session
 from api.main import app
 from api.settings import settings
 
-DATABASE_NAME = settings.DATABASE_URL.rsplit("/", 1)[-1]
-
-TEST_DATABASE_NAME = "test_" + DATABASE_NAME
-TEST_DATABASE_PORT = "5433"
-TEST_DATABASE_HOST = "test-database"
-TEST_DATABASE_URL = settings.DATABASE_URL.rsplit("/", 1)[0] + "/" + TEST_DATABASE_NAME
-TEST_DATABASE_URL = TEST_DATABASE_URL.replace("5432", TEST_DATABASE_PORT, 1)
-TEST_DATABASE_URL = TEST_DATABASE_URL.replace("database", TEST_DATABASE_HOST, 1)
-
 engine = AsyncEngine(
     create_engine(
-        TEST_DATABASE_URL,
+        settings.TEST_DATABASE_URL,
         echo=False,
         future=True,
     )
